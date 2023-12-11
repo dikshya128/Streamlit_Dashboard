d67a0f6fa7bc2b5ddae2c6eacea51dc2c9833674
dikshya128 <153480793+dikshya128@users.noreply.github.com>
2023-12-11 22:01:46 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,18 +8,18 @@ warnings.filterwarnings('ignore')
 st.set_page_config(page_title="Super Market", page_icon=":chart_with_upwards_trend:",layout="wide")
 
 st.title(" :chart_with_upwards_trend: Dashboard for Super Market")
-st.markdown('<style>div.block-container{padding-top:1 rem;}</style>',unsafe_allow_html=True)
+st.markdown('<style>div.block-container{padding-top:15px;}</style>',unsafe_allow_html=True)
 
-fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
-if fl is not None:
-    filename = fl.name
-    st.write(filename)
-    df = pd.read_csv(filename, encoding = "ISO-8859-1")
-else:
-    os.chdir(r"C:\Users\Asus\OneDrive\Documents\7th sem\Data Mining\StreamlitPractice")
-    df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
+# fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
+# if fl is not None:
+#     filename = fl.name
+#     st.write(filename)
+#     df = pd.read_csv(filename, encoding = "ISO-8859-1")
+# else:
+os.chdir(r"C:\Users\Asus\OneDrive\Documents\7th sem\Data Mining\StreamlitPractice")
+df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
 
-col1, col2 = st.columns((2))
+col1, col2 = st.columns((2)) #two columns for start date and end date
 df["Order Date"] = pd.to_datetime(df["Order Date"])
 
 # Getting the min and max date 
@@ -35,21 +35,21 @@ with col2:
 df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
 
 st.sidebar.header("Choose your filter: ")
-# Create for Region
+# For Region
 region = st.sidebar.multiselect("Pick your Region", df["Region"].unique())
 if not region:
     df2 = df.copy()
 else:
     df2 = df[df["Region"].isin(region)]
 
-# Create for State
+# For State
 state = st.sidebar.multiselect("Pick the State", df2["State"].unique())
 if not state:
     df3 = df2.copy()
 else:
     df3 = df2[df2["State"].isin(state)]
 
-# Create for City
+# For City
 city = st.sidebar.multiselect("Pick the City",df3["City"].unique())
 
 # Filter the data based on Region, State and City
@@ -71,36 +71,52 @@ elif city:
 else:
     filtered_df = df3[df3["Region"].isin(region) & df3["State"].isin(state) & df3["City"].isin(city)]
 
+
+
 category_df = filtered_df.groupby(by = ["Category"], as_index = False)["Sales"].sum()
+region_df = filtered_df.groupby(by = "Region", as_index = False)["Sales"].sum()
+
 
 with col1:
     st.subheader("Category wise Sales")
-    fig = px.bar(category_df, x = "Category", y = "Sales", text = ['${:,.2f}'.format(x) for x in category_df["Sales"]],
-                 template = "seaborn")
-    st.plotly_chart(fig,use_container_width=True, height = 200)
+    fig = px.pie(filtered_df, values = "Sales", names = "Category", hole = 0.6)
+    fig.update_traces(text = filtered_df["Category"], textposition = "inside",marker=dict(colors=['#EC6B56', '#FED60A', '#47B39C']))
+    st.plotly_chart(fig,use_container_width=True)
 
 with col2:
     st.subheader("Region wise Sales")
-    fig = px.pie(filtered_df, values = "Sales", names = "Region", hole = 0.5)
-    fig.update_traces(text = filtered_df["Region"], textposition = "outside")
+    fig = px.pie(filtered_df, values = "Sales", names = "Region", hole = 0.6)
+    fig.update_traces(text = filtered_df["Region"], textposition = "inside",marker=dict(colors=['#EC6B56', '#FED60A', '#47B39C','#1F75FE']))
     st.plotly_chart(fig,use_container_width=True)
 
 cl1, cl2 = st.columns((2))
 with cl1:
-    with st.expander("Category_ViewData"):
+    with st.expander("View Category Data"):
         st.write(category_df.style.background_gradient(cmap="Blues"))
         csv = category_df.to_csv(index = False).encode('utf-8')
         st.download_button("Download Data", data = csv, file_name = "Category.csv", mime = "text/csv",
                             help = 'Click here to download the data as a CSV file')
 
 with cl2:
-    with st.expander("Region_ViewData"):
-        region = filtered_df.groupby(by = "Region", as_index = False)["Sales"].sum()
-        st.write(region.style.background_gradient(cmap="Oranges"))
-        csv = region.to_csv(index = False).encode('utf-8')
+    with st.expander("View Region Data"):
+        st.write(region_df.style.background_gradient(cmap="Blues"))
+        csv = region_df.to_csv(index = False).encode('utf-8')
         st.download_button("Download Data", data = csv, file_name = "Region.csv", mime = "text/csv",
                         help = 'Click here to download the data as a CSV file')
-        
+
+Subcategory_df = filtered_df.groupby(by = ["Sub-Category"], as_index = False)["Sales"].sum()
+st.subheader("Sub-Category wise Sales") 
+fig = px.bar(Subcategory_df, x = "Sub-Category", y = "Sales", text = ['${:,.2f}'.format(x) for x in Subcategory_df["Sales"]], 
+                 template = "seaborn") 
+fig.update_traces(marker_color='#74BBFB')
+st.plotly_chart(fig,use_container_width=True, height = 200)
+
+with st.expander("View Sub-Category Data"):
+    st.write(Subcategory_df.style.background_gradient(cmap="Blues"))
+    csv = Subcategory_df.to_csv(index = False).encode('utf-8')
+    st.download_button("Download Data", data = csv, file_name = "Sub-Category.csv", mime = "text/csv",
+                            help = 'Click here to download the data as a CSV file')
+
 filtered_df["month_year"] = filtered_df["Order Date"].dt.to_period("M")
 st.subheader('Time Series Analysis')
 
@@ -153,7 +169,7 @@ data1['layout'].update(title="Relationship between Sales and Profits using Scatt
 st.plotly_chart(data1,use_container_width=True)
 
 with st.expander("View Data"):
-    st.write(filtered_df.iloc[:500,1:20:2].style.background_gradient(cmap="Oranges"))
+    st.write(filtered_df.iloc[:500,1:20:2].style.background_gradient(cmap="Blues"))
 
 # Download orginal DataSet
 csv = df.to_csv(index = False).encode('utf-8')
